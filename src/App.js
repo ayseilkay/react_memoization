4b019accd0544fa2b279f2cd6ed39237f4d54d24
ayseilkay <aayseilkay@gmail.com>
2022-01-04 22:18:41 +0300

    Memoization --> useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState,useMemo } from 'react';
+import { useState,useMemo,useCallback } from 'react';
 import './App.css';
 import Header from './components/Header';
 
@@ -14,17 +14,35 @@ function App() {
 
   const [number,setNumber]= useState(0);
   const data = useMemo(()=> {
-      return {name:"Ayse",number} // peki ne zaman bu data yeniden hesaplanacak? 
-  },[number]) // Ancak dependency array de belirtmis olunan veriler değiştiği anda hesaplanır.
+      return {name:"AYSE"} // peki ne zaman bu data yeniden hesaplanacak? 
+  },[]) // Ancak dependency array de belirtmis olunan veriler değiştiği anda hesaplanır.
+
+  const increment = useCallback(
+    () => {
+     setNumber((prevState)=> prevState +1 )//setNumber(number + 1) ı sildik preview hale getirdik
+    },
+    [],// burada number yazılırken sayı her degistiginde tekrar render ediliyordu. o yüzden numberı sildik
+  )
   return (
     <div className="App">
-      <Header number={number < 5 ? 0:number} data={data}/>
+      <Header increment={increment}/>
       <h1>{number}</h1>
-      <button onClick={()=> setNumber(number + 1)}>click</button>
+      {/* <button onClick={()=> setNumber(number + 1)}>click</button> */}
     </div>
   );
 }
-function calculateObject(){
-  return { name:"AYLIN"}
-}
+// // function calculateObject(){
+//   console.log("Calculating .....")
+//   for (let i = 0; i <1000000000; i++) {
+
+//   }
+//   console.log("Calculating Done...")
+//   return { name:"AYLIN"}
+
+// }
 export default App;
+
+
+
+// Toparlamak gerekirse;
+// Bir fonksiyonunuz varsa ve bu fonksiyonu herhangi bir alta geçiyorsanız. O alt componentın tekrar tekrar render olmasını istemiyorsanız fonksiyonlar özelinde useCallback kullanılır.
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,20 @@ import React from 'react'
 // Normal şartlarda React.memo ile sarmalanan bir yapıda biz prop geçmeden re render edilmesini önleyebiliyorduk.
 // Ama datayı prop olarak geçtiğimizde geçilen prop aynı olmasına rağmen React.memo ile sarılmış olmasına ragmen re-render meydana geliyor. Bunun sebebi {} === {}  yapısından false dönmesindendir.(referansları farklı)
 //Yani veri gözle görüldügünde aynı veri ama eşitliğe bakıdıgında öyle degil.
-function Header({number,data}) {
+function Header({number,data,increment}) {
     console.log("Header Component re-rendered")
     return (
         <div>
             <h1>Header - {number} </h1>
             <br/>
             <br/>
+            <button onClick={increment}>click Header</button>
             <code>{JSON.stringify(data)}</code>
         </div>
     )
 }
 
 export default React.memo(Header)
+
+
+//increment olarak verdiğmiz fonksiyonunun her defasında aynı oldugunu bir sekilde söylememiz gerekiyor.(useCallback)
