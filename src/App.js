e258add1b5db035b6d81300cfba65d063f365bda
ayseilkay <aayseilkay@gmail.com>
2022-01-04 21:51:10 +0300

    Memoization --> useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useMemo } from 'react';
 import './App.css';
 import Header from './components/Header';
 
@@ -6,17 +6,25 @@ import Header from './components/Header';
 // Eger componenta geçilen proplar degisirse o zaman yeniden render edilir.
 
 // React.memo() yapısında değişen bir prop yoksa render gerçekleşmez. Ama prop da değişim varsa  render gerçekleşir
+
+// datayı ekledigimizde {} === {} referans farklılıgından dolayı false döner.
+// Bu nedenle  const data = {name:"Ayse"} yapısını dısarıya alıyoruz
+// const data = {name:"Ayse"}//bunu dısarda yazamıyorsak illa içerde tanımlamamız gerekiyor ise(useMemo kullanılır)
 function App() {
 
   const [number,setNumber]= useState(0);
-
+  const data = useMemo(()=> {
+      return {name:"Ayse",number} // peki ne zaman bu data yeniden hesaplanacak? 
+  },[number]) // Ancak dependency array de belirtmis olunan veriler değiştiği anda hesaplanır.
   return (
     <div className="App">
-      <Header number={number < 5 ? 0:number}/>
+      <Header number={number < 5 ? 0:number} data={data}/>
       <h1>{number}</h1>
       <button onClick={()=> setNumber(number + 1)}>click</button>
     </div>
   );
 }
-
+function calculateObject(){
+  return { name:"AYLIN"}
+}
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
-
-function Header({number}) {
+// Normal şartlarda React.memo ile sarmalanan bir yapıda biz prop geçmeden re render edilmesini önleyebiliyorduk.
+// Ama datayı prop olarak geçtiğimizde geçilen prop aynı olmasına rağmen React.memo ile sarılmış olmasına ragmen re-render meydana geliyor. Bunun sebebi {} === {}  yapısından false dönmesindendir.(referansları farklı)
+//Yani veri gözle görüldügünde aynı veri ama eşitliğe bakıdıgında öyle degil.
+function Header({number,data}) {
     console.log("Header Component re-rendered")
     return (
         <div>
-            <h1>Header - {number}</h1>
+            <h1>Header - {number} </h1>
+            <br/>
+            <br/>
+            <code>{JSON.stringify(data)}</code>
         </div>
     )
 }
